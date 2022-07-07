c1d2c810662b9ba7a6b58bbb4e299870a18a660b
Kangheamin <rjwlspfh@naver.com>
2022-07-08 01:56:50 +0900

    first

---
+++ js/main.js
@@ -0,0 +1,142 @@
+'use strict'
+//Make nalet transparent when it is on the top 
+const navbar = document.querySelector('#navber'),
+    navbarHeight =navbar.getBoundingClientRect().height;
+
+document.addEventListener('scroll', function(){
+    if(window.scrollY > navbarHeight ){
+        navbar.classList.add('navbar_dark');
+    }
+    else{
+        navbar.classList.remove('navbar_dark');
+    }
+});
+//Handle scrolling when tapping on the navbar menu
+
+const navbarMenu = document.querySelector('.navber_menu');
+navbarMenu.addEventListener('click', (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  navbarMenu.classList.remove('open');
+  scrollIntoView(link);
+});
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar_toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open');
+});
+
+//handle click on "contactme" button on home
+const homeContactBtn = document.querySelector('.home_contact');
+
+homeContactBtn.addEventListener('click', () => {
+    scrollIntoView('#contact');
+});
+
+//scroll home
+const home = document.querySelector('.home_container');
+const homeHeight = home.getBoundingClientRect().height;
+
+document.addEventListener('scroll',()=>{
+    home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+//Show "top button" when scrolling down
+const topbutton = document.querySelector('.topbutton');
+
+document.addEventListener('scroll',()=>{
+    if (window.scrollY > homeHeight) {
+        topbutton.classList.add('visible');
+    } else{
+        topbutton.classList.remove('visible');
+    }
+});
+
+topbutton.addEventListener('click',()=>{
+    scrollIntoView('#home');
+});
+
+//Projects
+const workBtnContainer = document.querySelector('.work_categories');
+const projectContainer = document.querySelector('.work_projects');
+const projects = document.querySelectorAll('.project');
+/* 💩💩💩 구식 버전 💩💩💩
+workBtnContainer.addEventListener('click', (e)=>{
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+    if(filter == null) {
+        return;
+    }
+
+//Remove selection from the previous item and select the new one
+const active = document.querySelector('.category_btn.selected');
+active.classList.remove('selected');
+
+const target = e.target.parentNode === 'BUTTON' ? e.targer : e.target.parentNode;
+                e.target.classList.add('selected');
+target.classList.add('selected');
+
+    projectContainer.classList.add('anim-out');
+       setTimeout(()=>{
+        projects.forEach((project) => {
+            console.log(project.dataset.type);
+            if(filter ==='*' || filter === project.dataset.type) {
+                project.classList.remove('invisible');
+            }else {
+                project.classList.add('invisible');
+            }
+        }); 
+        projectContainer.classList.remove('anim-out')
+    }, 300);
+
+});
+💩💩💩💩*/
+
+// funtion 
+function scrollIntoView(selector){
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior: "smooth"});
+}
+
+//제이쿼리 부분
+$(document).ready(function(){
+
+
+    //stroke-dashoffset: calc(440 - 440 * 95 / 100);
+
+    let startCount = 0;
+    
+
+    $("#circle_bar .box").each(function(index){
+        //console.log(index);
+
+        let sel_count = $(this).find(".count").attr("date-limit");
+        console.log(sel_count);
+
+        $(this).find("circle:eq(1)").css("stroke-dashoffset", `calc(440 - 440 * ${sel_count} / 100 )`);
+    
+        let counter = setInterval(function(){
+            if(startCount < sel_count){
+                startCount++;
+                $(".box").eq(index).find(".count").text(startCount);
+            }else if(startCount == sel_count){ //startCount++ 도달 되었으나 값이 적용 X 최종적으로 현재 저장된 date-limit의 값을 넣어줌
+                $(".box").eq(index).find(".count").text(sel_count);
+            }else{
+                clearInterval(counter);
+            }
+        },80);
+    
+    });
+
+            //구글 이메일 hide 보냈을때
+
+            $(".close").click(function(){
+                $(".thankyou_message").hide();
+                return false;
+            });
+            
+});
+
+
---
+++ js/work.js
@@ -0,0 +1,65 @@
+//✨💕 Work 신세대 부분 💕✨
+console.log("hi");
+const photoArr = [
+    ["kuliner.png", "Kuliner website", "HTML, CSS, JQUERY를 이용하여 PC버전으로 제작되었습니다.","web", "https://kangheamin.github.io/kuliner/"],
+    ["cakehouse.png", "cakehouse", "HTML, CSS, JQUERY를 이용하여 PC버전으로 제작되었습니다.","web", "https://kangheamin.github.io/CakeHouse/"],
+    ["cashmere.png", "cashmere", "HTML, CSS, JQUERY를 이용하여 PC버전으로 제작되었습니다.","web", "https://kangheamin.github.io/wedding/"],
+    ["hospital.png", "hospital", "HTML, CSS, JQUERY를 이용하여 PC버전으로 제작되었습니다.","web", "https://kangheamin.github.io/Hospital/"],
+    ["origin.png", "origin", "HTML, CSS, JQUERY를 이용하여 PC버전으로 제작되었습니다.","web", "https://kangheamin.github.io/Origin/"],
+    ["bnostore.png", "B&O", "HTML, CSS, JQUERY를 이용하여 PC버전으로 제작되었습니다.","web", "https://kangheamin.github.io/wedding/"],
+    ["music.png", "Music", "HTML, CSS, JQUERY를 이용하여 PC버전으로 제작되었습니다.","web", "https://kangheamin.github.io/music/"],
+    ["movie_ajax.png", "Jangsari", "HTML, CSS, JQUERY를 이용하여 PC버전으로 제작되었습니다.","ajax", "https://kangheamin.github.io/ajax_Jangsari/"],
+    ["weatherApp.png", "weatherApp", "HTML, CSS, JQUERY를 이용하여 PC버전으로 제작되었습니다.","ajax", "https://kangheamin.github.io/wedding/"],
+    ["national_geographic.png", "national_geographic", "HTML, CSS, JQUERY를 이용하여 PC버전으로 제작되었습니다.","vue", "https://ngeo0111.netlify.app/#/"],
+    ["audition_vue.png", "audition_vue", "Vue를 이용하여 PC버전으로 제작되었습니다.","vue", "https://kangheamin.github.io/vue_audition/"],
+    ["chatting.png", "B&O", "HTML, CSS, JQUERY를 이용하여 PC버전으로 제작되었습니다.","php", "http://bobpop20.dothome.co.kr/chattingApp/login.html"],
+    ["oclass.png", "B&O", "HTML, CSS, JQUERY를 이용하여 PC버전으로 제작되었습니다.","php", "http://bobpop20.dothome.co.kr/oclass/"],
+    ["ecomona.png", "ecomona", "HTML, CSS, Jacascript를 이용하여 PC버전으로 제작되었습니다.","develop", "https://kangheamin.github.io/ecomona/"],
+    ["game.png", "ecomona", "HTML, CSS, Jacascript를 이용하여 PC버전으로 제작되었습니다.","develop", "https://kangheamin.github.io/minigame/"],
+]
+
+const sectionSpace = document.querySelector("#grid_system");
+let sectionCont = "";
+
+for(v of photoArr){
+    sectionCont += `
+    <article class="${v[3]}">
+        <div class="photo" onclick="window.open('${v[4]}')">
+        <div class="img" style="background-image:url(./image/${v[0]})" src="${v[4]}"></div>
+            <h3>${v[1]}</h3>
+            <p>${v[2]}</p>
+            <a class="bluecolor" href="${v[4]}" target="_blank"><i class="fas fa-arrow-right">&nbsp;</i>Detail_view</a>
+        </div>
+    </article>
+    `
+}
+sectionSpace.innerHTML = sectionCont;
+
+let grid;
+const timeOut = setTimeout(() => {
+    grid = new Isotope("#grid_system", {
+        itemSelector : "article",
+        transitionDuration : "0.5s"
+    });
+}, 500);
+
+//클릭한 곳의 이미지를 보여주는 구현
+const sortBtns = document.querySelectorAll(".sort_btn li");
+
+for(v of sortBtns){
+    v.addEventListener("click", (e) => {
+        e.preventDefault();
+        const dataSort = e.currentTarget.querySelector("a").getAttribute("data-sort");
+        grid.arrange({
+            filter : dataSort
+        });
+
+        //전체 버튼에 클래스명 active 제거
+        for(v of sortBtns){
+            v.classList.remove("active");
+        }
+
+        //클릭한 곳의 버튼만 클래스명 active 부여
+        e.currentTarget.classList.add("active");
+    });    
+}
